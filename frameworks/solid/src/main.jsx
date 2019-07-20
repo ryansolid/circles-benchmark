32ed5c71f6e7e3d9cc7602cdfd2ed4622a1f7872
Ryan Carniato <ryansolid@gmail.com>
2019-07-19 23:59:42 -0700

    update solid

--- frameworks/solid/src/main.jsx
+++ frameworks/solid/src/main.jsx
@@ -1,4 +1,5 @@
-import { createRoot, createState } from 'solid-js';
+import { createState } from 'solid-js';
+import { render } from 'solid-js/dom';
 
 function createBoxes(number) {
   const boxes = [];
@@ -10,7 +11,7 @@ function createBoxes(number) {
 function tick(box) {
   const count = box.count + 1;
   return {
-    top:  Math.sin(count / 10) * 10,
+    top: Math.sin(count / 10) * 10,
     left: Math.cos(count / 10) * 10,
     color: count % 255,
     content: count % 100,
@@ -24,27 +25,22 @@ const Main = () => {
   Benchmark.Framework.Solid.loop = () =>
     Promise.resolve().then(() => setState('boxes', '*', tick));
 
-  return <>{
-    state.boxes.map((box, index) =>
-      <div class="box-view">
-        <div class="box" id={index} style={({
-            top: `${box.top}px`,
-            left: `${box.left}px`,
-            background: `rgb(0,0,${box.color})`
-          })}
-        >{(box.content)}</div>
-      </div>
-    )
-  }</>
+  return state.boxes.map((box, index) =>
+    <div class="box-view">
+      <div class="box" id={index} style={({
+        top: `${box.top}px`,
+        left: `${box.left}px`,
+        background: `rgb(0,0,${box.color})`
+      })}
+      >{(box.content)}</div>
+    </div>
+  );
 }
 
 let dispose;
 Benchmark.Framework.Solid = {
   start() {
-    createRoot(disposer => {
-      dispose = disposer;
-      document.getElementById('grid').appendChild(<Main />);
-    })
+    dispose = render(Main, document.getElementById('grid'));
   },
   cleanup() { dispose && dispose(); }
 }
--- frameworks/solidsignals/src/main.jsx
+++ frameworks/solidsignals/src/main.jsx
@@ -1,4 +1,5 @@
-import { createRoot, createSignal, freeze } from 'solid-js';
+import { createSignal, freeze } from 'solid-js';
+import { render } from 'solid-js/dom';
 
 function createBoxes(number) {
   const boxes = [];
@@ -8,7 +9,6 @@ function createBoxes(number) {
       [color, setColor] = createSignal(null),
       [content, setContent] = createSignal(0);
     boxes.push({ top, left, color, content, setTop, setLeft, setColor, setContent, count: 0 });
-
   }
   return boxes;
 }
@@ -27,27 +27,22 @@ const Main = () => {
   Benchmark.Framework.SolidSignals.loop = () =>
     Promise.resolve().then(() => freeze(() => boxes.forEach(tick)));
 
-  return <>{
-    boxes.map((box, index) =>
-      <div class="box-view">
-        <div class="box" id={index} style={({
-            top: `${box.top()}px`,
-            left: `${box.left()}px`,
-            background: `rgb(0,0,${box.color()})`
-          })}
-        >{box.content}</div>
-      </div>
-    )
-  }</>
+  return boxes.map((box, index) =>
+    <div class="box-view">
+      <div class="box" id={index} style={({
+        top: `${box.top()}px`,
+        left: `${box.left()}px`,
+        background: `rgb(0,0,${box.color()})`
+      })}
+      >{box.content}</div>
+    </div>
+  );
 }
 
 let dispose;
 Benchmark.Framework.SolidSignals = {
   start() {
-    createRoot(disposer => {
-      dispose = disposer;
-      document.getElementById('grid').appendChild(<Main />);
-    })
+    dispose = render(Main, document.getElementById('grid'));
   },
   cleanup() { dispose && dispose(); }
 }
